f1e979ba04d00d70f3a736ef11d35119f02a41d3
VietAnhNguyenNam <120655630+VietAnhNguyenNam@users.noreply.github.com>
2023-10-26 22:23:21 +0700

    frontend demo

---
+++ fastfood/script/script.js
@@ -0,0 +1,60 @@
+// tabs manipulation on tab bar
+function openTab(tab) {
+    // window.open("home.html");   // open new tab
+    window.location.href = tab + '.html';   // reload current tab
+}
+
+// manual timing carousel
+{
+// let currentSlide = 0;
+// const slideInterval = 3000;  // Change slide every 3 seconds (adjust as needed)
+// let slideTimer;
+
+// function showSlide(index) {
+//     const slides = document.querySelectorAll('.carousel');
+
+//     if (index >= slides.length) {
+//         currentSlide = 0;
+//     } else if (index < 0) {
+//         currentSlide = slides.length - 1;
+//     } else {
+//         currentSlide = index;
+//     }
+
+//     slides.forEach((slide) => {
+//         slide.style.display = 'none';
+//     });
+
+//     slides[currentSlide].style.display = 'block';
+// }
+
+// function prevSlide() {
+//     showSlide(currentSlide - 1);
+//     delayCarousel();
+// }
+
+// function nextSlide() {
+//     showSlide(currentSlide + 1);
+//     delayCarousel();
+// }
+
+// function startCarousel() {
+//     slideTimer = setInterval(() => {
+//         showSlide(currentSlide + 1);
+//     }, slideInterval);
+// }
+
+// function stopCarousel() {
+//     clearInterval(slideTimer);
+// }
+
+// function delayCarousel() {
+//     stopCarousel();
+//     startCarousel();
+//     // startCarousel delay for a while at first itself
+// }
+
+// // Show the first slide initially
+// showSlide(currentSlide);
+// startCarousel();
+}
